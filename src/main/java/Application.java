1469af1fbd80a93d1fbdaeb26e2f7c70f9f9a813
李林 <3086961507@qq.com>
2020-01-13 20:13:47 +0800

    打成第三方jar

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -9,40 +9,40 @@ import java.util.concurrent.Executors;
 
 public class Application {
     public static void main(String[] args) throws InterruptedException {
-        Node node = Node.getInstance();
-        node.init(
-                //47.105.38.255
-                "http://127.0.0.1:9050",
-                "bifu-btc",
-                "LKJIUJNBNDSB4561324AUYG",
-                "127.0.0.1",
-                9050
-        );
-
-        HttpClient.getInstance().setCredentials(
-                node.getCredentialsUserName(),
-                node.getCredentialsPassword(),
-                node.getAuthScopeHost(),
-                node.getAuthScopePort());
-
-        BlockScanner scanner = new BlockScanner();
-//        ExecutorService threadPool=Executors.newFixedThreadPool(5);
-        for (int blockHeight = 607447; blockHeight < 607592; blockHeight++) {
-            int finalBlockHeight = blockHeight;
-//            threadPool.submit(new Runnable() {
-//                @Override
-//                public void run() {
-                    long startTime = System.currentTimeMillis();
-                    TransactionQueue transactionQueue = scanner.scan(finalBlockHeight);
-                    while (!transactionQueue.productFinished()) {
-                        JSONObject transaction = transactionQueue.take();
-                    }
-                    System.out.printf("Block height:" + finalBlockHeight + " tx count:" + scanner.getBlock().getTx().size() + " 耗时：%d ms\n", System.currentTimeMillis() - startTime);
-                    System.out.println("scan finished.");
-                }
-//            });
-//        }
-//        Thread.currentThread().join();
-        scanner.shutdown();
+//        Node node = Node.getInstance();
+//        node.init(
+//                //47.105.38.255
+//                "http://127.0.0.1:9050",
+//                "bifu-btc",
+//                "LKJIUJNBNDSB4561324AUYG",
+//                "127.0.0.1",
+//                9050
+//        );
+//
+//        HttpClient.getInstance().setCredentials(
+//                node.getCredentialsUserName(),
+//                node.getCredentialsPassword(),
+//                node.getAuthScopeHost(),
+//                node.getAuthScopePort());
+//
+//        BlockScanner scanner = new BlockScanner();
+////        ExecutorService threadPool=Executors.newFixedThreadPool(5);
+//        for (int blockHeight = 607447; blockHeight < 607592; blockHeight++) {
+//            int finalBlockHeight = blockHeight;
+////            threadPool.submit(new Runnable() {
+////                @Override
+////                public void run() {
+//                    long startTime = System.currentTimeMillis();
+//                    TransactionQueue transactionQueue = scanner.scan(finalBlockHeight);
+//                    while (!transactionQueue.productFinished()) {
+//                        JSONObject transaction = transactionQueue.take();
+//                    }
+//                    System.out.printf("Block height:" + finalBlockHeight + " tx count:" + scanner.getBlock().getTx().size() + " 耗时：%d ms\n", System.currentTimeMillis() - startTime);
+//                    System.out.println("scan finished.");
+//                }
+////            });
+////        }
+////        Thread.currentThread().join();
+//        scanner.shutdown();
     }
 }
